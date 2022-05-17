3470302ca93cdaf910c741febf34fb7efa849cbc
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-05-17 15:38:35 +0530

    feat: random image

--- src/utils/greetings.js
+++ src/utils/greetings.js
@@ -1,11 +1,11 @@
 export const greetings = (currentHour) => {
 	if (currentHour >= 1 && currentHour < 12) {
 		return "Good Morning";
-	} else if (currentHour >= 12 && currentHour < 15) {
+	} else if (currentHour >= 12 && currentHour < 17) {
 		return "Good Afternoon";
-	} else if (currentHour >= 15 && currentHour < 17) {
-		return "Good Evening";
 	} else if (currentHour >= 17 && currentHour < 19) {
+		return "Good Evening";
+	} else if (currentHour >= 19 && currentHour < 24) {
 		return "Good Night";
 	} else {
 		return "Hello";
