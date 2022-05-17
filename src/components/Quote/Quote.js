859e0aabd6986e858706ac98571b463770fce177
Shraddha Muley <66071175+shraddha15-creator@users.noreply.github.com>
2022-05-17 15:59:40 +0530

    feat: random quotes

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,13 +1,32 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./quote.css";
 export const Quote = () => {
+	const [quote, setQuote] = useState("");
+	const [author, setAuthor] = useState("");
+
+	const getRandomQuote = async () => {
+		try {
+			const response = await axios.get(
+				"https://goquotes-api.herokuapp.com/api/v1/random?count=1"
+			);
+			setQuote(response.data.quotes[0].text);
+			setAuthor(response.data.quotes[0].author);
+			console.log(response.data.quotes);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getRandomQuote();
+	}, []);
+
 	return (
 		<>
 			<div className="quote-and-author">
-				<p className="quote">
-					“The secret of getting ahead is getting started.”
-				</p>
-				<p className="quote-author">- Mark Twain</p>
+				<p className="quote">“{quote}”</p>
+				<p className="quote-author">- {author}</p>
 			</div>
 		</>
 	);
